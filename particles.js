670d32fe6f3bdcb74183a7ea27f6623e5ec44391
Rovers,Zjuul Z <z.rovers@student.fontys.nl>
2021-04-26 14:38:35 +0200

    Animatie

--- particles.js
+++ particles.js
@@ -9,8 +9,17 @@ document.addEventListener('DOMContentLoaded', () => {
             targets: ".particle",
             translateX: [0, 280],
             rotate: 45,
-            duration: 1000,
-            easing: 'spring(1, 80, 10, 0)',
+            easing: 'spring(1, 80, 60, 0)',
+            delay: function() {
+                return anime.random(0, 500);
+              },
+        })
+
+        anime({
+            targets: ".particle2",
+            translateX: [0, 560],
+            rotate: 45,
+            easing: 'spring(1, 80, 60, 0)',
             delay: function() {
                 return anime.random(0, 500);
               },
